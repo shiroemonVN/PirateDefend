2167d701075bb29fc0aa4e7944b27473bafd5e14
longdv-tt <shiroemonvn@gmail.com>
2015-12-03 21:08:42 +0700

    Maybay
    Sua excel

---
+++ Assets/Health.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    Image img;
+	// Use this for initialization
+	void Start () {
+        img = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.forward = Camera.main.transform.forward;
+	}
+}
