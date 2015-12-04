c8f69892fffbf197ca42e57176245144b6ceaee2
hungbm <hungbmit@gmail.com>
2015-12-04 11:50:02 +0700

    add Price

---
+++ Assets/ButtonNextLVL.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonNextLVL : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void LoadNextLevel(int level)
+    {
+        if (level == 0 || level == null)
+        {
+            Application.LoadLevel("HighScore");
+        }
+        else   Application.LoadLevel("Level"+level);
+    }
+
+}
