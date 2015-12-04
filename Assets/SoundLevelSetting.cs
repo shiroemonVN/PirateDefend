921cf56e86d358a02617d206cbb090a72e6e84f0
longdv-tt <shiroemonvn@gmail.com>
2015-12-04 07:07:06 +0700

    Complete Sound Setting
    Done Sound Setting

---
+++ Assets/SoundLevelSetting.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundLevelSetting : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        float s = PlayerPrefs.GetFloat("SoundVolume");
+        GetComponent<AudioSource>().volume = s;
+	}
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+    public void SoundSetting()
+    {
+        GetComponent<AudioSource>().volume = 0.5f;
+    }
+}
